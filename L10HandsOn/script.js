909c95d9ab52d6b5f084463ab4808cef547e014e
jltraveler <103217708+jltraveler@users.noreply.github.com>
2022-04-30 12:32:41 -0400

    Add files via upload

---
+++ L10HandsOn/script.js
@@ -0,0 +1,16 @@
+// $(function() {
+//     $('.icon').hover(function() {
+//      $(this).css({ width: "150%", height: "150%" });
+//      }, 
+//        function () {
+//             $(this).css({ width: "100%", height: "100%" });
+//     });
+// });
+$(() => {
+  $('.icon').hover(function() {
+   $(this).css({ width: "150%", height: "150%" });
+   }, 
+      function () {
+          $(this).css({ width: "90%", height: "90%" });
+  });
+});
